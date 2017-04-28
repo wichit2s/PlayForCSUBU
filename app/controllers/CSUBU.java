f0edf2b29c3e0ecfacd2053433eb1878110e4d91
Paul <wichit.s@ubu.ac.th>
2017-04-28 18:16:00 +0700

    add upload file

--- app/controllers/CSUBU.java
+++ app/controllers/CSUBU.java
@@ -1,13 +1,43 @@
 package controllers;
 
+import play.Logger;
 import play.mvc.*;
+import play.mvc.Http.*;
+import play.mvc.Http.MultipartFormData.*;
+import play.data.*;
+
+import java.io.*;
 
 import views.html.*;
 
 public class CSUBU extends Controller {
 
+  private Logger.ALogger log = Logger.of(CSUBU.class);
+
   public Result index() {
     return ok(csubu.render());
+  }
+
+  public Result upload() {
+    MultipartFormData<File> body = request().body().asMultipartFormData();
+    FilePart<File> picture = body.getFile("picture");
+    if (picture != null) {
+        String fileName = picture.getFilename();
+        String contentType = picture.getContentType();
+        File file = picture.getFile();
+        log.warn("upload file is at: {}", file.getAbsolutePath());
+        try {
+          File upload = new File("public/images/gotImages/"+fileName);
+          log.warn("renameto: {}", upload.getAbsolutePath());
+          file.renameTo(new File("public/images/gotImages/"+fileName));
+        } catch (Exception ex) {
+          log.warn(ex.toString());
+        }
+        return redirect("/");
+    } else {
+        flash("error", "Missing file");
+        return badRequest();
+    }
   }
 
 }
